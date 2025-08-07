050c76d3d1a48c0b3ac12861a240db16e753ef38
shivam <shivam.neer.shah@gmail.com>
2025-08-07 16:15:06 +0530

    added captcha, google tag and microsoft clarity

--- _data/env.js
+++ _data/env.js
@@ -1,6 +1,6 @@
 module.exports = {
-    GA4_TRACKING_ID: process.env.GA4_TRACKING_ID || 'G-P2YM46QZCK',
+    GA4_TRACKING_ID: process.env.GA4_TRACKING_ID || 'G-GBTY565EQ7',
     CLARITY_PROJECT_ID: process.env.CLARITY_PROJECT_ID || 'sr1y78mww9',
-    RECAPTCHA_SITE_KEY: process.env.RECAPTCHA_SITE_KEY || '6Lc8k2oqAAAAAF9QKbYgHmrJ2k5vQ0zEWlYRfK7k',
+    RECAPTCHA_SITE_KEY: process.env.RECAPTCHA_SITE_KEY || '6Ld9bp0rAAAAACdIYemp9LvEyC6NGghMjeyUkR0u',
     NODE_ENV: process.env.NODE_ENV || 'development'
 };
